01cb1a6229bc0bfcbdb397d72982b9d048d274fc
yuri s <ggyury1@gmail.com>
2019-08-05 16:12:04 +0200

    add readme file

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import str from './models/API';
+import {
+    add,
+    mult,
+    minus
+} from './models/API';
+console.log(str);
+console.log(`add= ${add(1,2)}\nmult= ${mult(2,3)}\nminus= ${minus(1,1)}`);
---
+++ src/js/models/API.js
@@ -0,0 +1,11 @@
+import {
+    add,
+    mult
+} from '../views/searchView';
+export {
+    add,
+    mult,
+    minus
+}
+const minus = (a, b) => a - b;
+export default 'Exported string';
